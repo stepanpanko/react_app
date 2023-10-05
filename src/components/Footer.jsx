0362f2c04f672ee634558d74b2bf3535b73de546
stepanpanko <stepanpanko55@gmail.com>
2023-10-05 15:16:43 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-25">
+      © 2023 All rights reserved by <span className='font-bold'>Dashy.com</span>
+    </p>
+  </div>
+);
+
+export default Footer;
